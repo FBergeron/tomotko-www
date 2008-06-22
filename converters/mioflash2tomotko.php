5f2ab608cb8daade3dc9d628abbeca93f4978475
Frederic Bergeron <fbergeron@users.sourceforge.net>
2008-06-22 08:02:34 +0000

    Updated MioFlash conversion script to new format.

--- converters/mioflash2tomotko.php
+++ converters/mioflash2tomotko.php
@@ -1,6 +1,7 @@
 <?php
-$cmd = "/home/groups/t/to/tomotko/cgi-bin/convertMioFlash2ToMOTko.rb < " . $_FILES[ 'glossaryFile' ][ 'tmp_name' ];
-header('Content-type: application/xml');
-header('Content-disposition: attachment; filename="glossary.xml"');
-system( $cmd );
+$cmd = "/home/groups/t/to/tomotko/cgi-bin/convertMioFlash2ToMOTko2.rb < " . $_FILES[ 'glossaryFile' ][ 'tmp_name' ];
+
+header('Content-type: application/zip');
+header('Content-disposition: attachment; filename="glossary.zip"');
+passthru( $cmd );
 ?>
