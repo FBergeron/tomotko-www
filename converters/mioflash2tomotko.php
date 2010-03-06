d0c05903f447c495ba4868654bd5e6a6d668584f
Frederic Bergeron <fbergeron@users.sourceforge.net>
2010-03-06 13:41:06 +0000

    Fixed erroneous script name.

--- converters/mioflash2tomotko.php
+++ converters/mioflash2tomotko.php
@@ -1,5 +1,5 @@
 <?php
-$cmd = "/home/groups/t/to/tomotko/cgi-bin/convertMioFlash2ToMOTko2.rb < " . $_FILES[ 'glossaryFile' ][ 'tmp_name' ];
+$cmd = "/home/groups/t/to/tomotko/cgi-bin/convertMioFlash2ToMOTko.rb < " . $_FILES[ 'glossaryFile' ][ 'tmp_name' ];
 
 header('Content-type: application/zip');
 header('Content-disposition: attachment; filename="glossary.zip"');
